1871f9930154d9d5d643f7a2b8f19b27263eabf1
balakancharla <155118961+balakancharla@users.noreply.github.com>
2026-01-30 17:34:11 -0600

    Change S3 bucket name for Terraform backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-ec2-jenkins-state"   # Update with your bucket name
+    bucket = "terraform-ec2-jenkins-state-file"   # Update with your bucket name
     key    = "ec2-instance/terraform.tfstate"
     region = "us-east-2"
   }
